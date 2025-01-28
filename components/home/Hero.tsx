74732b221c0822d8f8070f45c027cc90ade4c299
Maciek <maciejsdev@gmail.com>
2025-01-28 15:14:07 +0100

    hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,26 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop.
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti et
+          enim commodi vel, eaque officia veritatis mollitia animi quia dolorum
+          fugiat repellat asperiores iusto, reprehenderit quam quasi reiciendis
+          doloribus laborum.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 export default Hero;
