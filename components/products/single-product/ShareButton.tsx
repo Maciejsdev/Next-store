1352079070abb640b1ce8b5d2fcbb02b000e12de
Maciek <maciejsdev@gmail.com>
2025-01-31 13:37:00 +0100

    Share feature

---
+++ components/products/single-product/ShareButton.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { LuShare2 } from "react-icons/lu";
+
+import {
+  TwitterShareButton,
+  EmailShareButton,
+  LinkedinShareButton,
+  TwitterIcon,
+  EmailIcon,
+  LinkedinIcon,
+} from "react-share";
+
+function ShareButton({ productId, name }: { productId: string; name: string }) {
+  const url = process.env.NEXT_PUBLIC_WEBSITE_URL;
+  const shareLink = `${url}/product/${productId}`;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size="icon" className="p-2">
+          <LuShare2 />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        side="top"
+        align="end"
+        sideOffset={10}
+        className="flex items-center gap-x-2 justify-center w-full"
+      >
+        <TwitterShareButton url={shareLink} title={name}>
+          <TwitterIcon size={24} round />
+        </TwitterShareButton>
+        <LinkedinShareButton url={shareLink} title={name}>
+          <LinkedinIcon size={24} round />
+        </LinkedinShareButton>
+        <EmailShareButton url={shareLink} title={name}>
+          <EmailIcon size={24} round />
+        </EmailShareButton>
+      </PopoverContent>
+    </Popover>
+  );
+}
+export default ShareButton;
