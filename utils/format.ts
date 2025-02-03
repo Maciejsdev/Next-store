350756f6b3113b97d79b0f6e140c6c763896832e
Maciek <maciejsdev@gmail.com>
2025-02-03 13:03:50 +0100

    sales, orders functionality added w/o proceeding to payment

--- utils/format.ts
+++ utils/format.ts
@@ -5,3 +5,11 @@ export const formatCurrency = (amount: number | null) => {
     currency: "EUR",
   }).format(value);
 };
+
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-IE", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+};
